74b9b231dc240cf4d85c73b78c93ec659b8a91d4
Warrick Holfeld <warrick.holfeld@envestnet.com>
2018-05-15 00:17:47 -0700

    finished project

---
+++ src/components/posts_new.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
+class PostsNew extends Component {
+  renderField(field) {
+      const { meta: { touched, error } } = field;
+      const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
+      return (
+        <div className={className}>
+          <label>{ field.label }</label>
+          <input
+            className="form-control"
+            type="text"
+            {...field.input}
+          />
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
+        </div>
+      );
+  }
+
+  onSubmit(values) {
+    // this === component
+    //this.props.history.push('/');
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title For Post"
+          name="title"
+          component={this.renderField}
+          />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+      <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      <button type="submit" className="btn btn-primary">Submit</button>
+      <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    );
+  }
+}
+
+function validate(values) {
+  // console.log(values) -> { title: 'asdf', categories: '', content: 'asdf' }
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  // if (values.title.length() < 3) {
+  //   errors.title = 'Title must be at least 3 characters';
+  // }
+  if (!values.title) {
+    errors.title = "enter a title!";
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content please';
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If erros has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(
+  connect(null,{ createPost })(PostsNew)
+);
